dd69ce326509d0e14afd8ca86ccc91af87a2ca55
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-01 16:49:12 -0400

    keep getting errors, moving on to a different project

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyttsx3, pdfplumber
+
+pdfreader = pdfplumber(open('SA.pdf', 'rb'))
+
+# Initiate Speaker
+speaker = pyttsx3.init()
+
+for page_num in range(pdfreader.numPages):
+    text = pdfreader.getPage(page_num).extractText() #extract text
+    clean_text = text.strip().replace('\n', ' ')
+    print(clean_text)
+
+speaker.save_to_file(clean_text, 'story.mp3')
+speaker.runAndWait()
+
+speaker.stop()
